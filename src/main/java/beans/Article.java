0dc41eae9cb4b9395bf287c4a8e1d5f93d299501
Pauline Andrzejewski <pauline.andrzejewski@gmail.com>
2022-01-25 13:56:09 +0100

    création des beans

---
+++ src/main/java/beans/Article.java
@@ -0,0 +1,10 @@
+package beans;
+
+public class Article {
+	
+	private String titre;
+	private String description;
+	private String contenu;
+	
+
+}
---
+++ src/main/java/beans/Utilisateur.java
@@ -0,0 +1,37 @@
+package beans;
+
+public class Utilisateur {
+	private String nom;
+	private String prenom;
+	private String email;
+	
+	public Utilisateur() {
+		super();
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
