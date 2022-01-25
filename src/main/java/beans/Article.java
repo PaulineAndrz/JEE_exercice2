5e5407b7424f6f9eb20d4c59ee09a2b63386dfcc
Pauline Andrzejewski <pauline.andrzejewski@gmail.com>
2022-01-25 14:03:31 +0100

    création des beans

--- src/main/java/beans/Article.java
+++ src/main/java/beans/Article.java
@@ -5,6 +5,42 @@ public class Article {
 	private String titre;
 	private String description;
 	private String contenu;
+	private Utilisateur auteur;
 	
+	public Article() {
+		super();
+	}
 
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+
+	public Utilisateur getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(Utilisateur auteur) {
+		this.auteur = auteur;
+	}
+	
 }
